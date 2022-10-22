c74bad53e5550d296211ec68fc598884d29b4df1
harukon <89727720+KonnoHaruto@users.noreply.github.com>
2022-10-22 18:21:30 +0900

    initial commit

---
+++ bin/my_first_bot.dart
@@ -0,0 +1,5 @@
+import 'package:my_first_bot/my_first_bot.dart' as my_first_bot;
+
+void main(List<String> arguments) {
+  print('Hello world: ${my_first_bot.calculate()}!');
+}
---
+++ test/my_first_bot_test.dart
@@ -0,0 +1,8 @@
+import 'package:my_first_bot/my_first_bot.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
